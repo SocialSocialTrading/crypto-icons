d507e1c0cdadd7d20c320cd5f015f9741ea02a48
assc-ryan <112598768+assc-ryan@users.noreply.github.com>
2023-01-12 17:48:22 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,9 @@
 const BASE_URL = "https://raw.githubusercontent.com/SocialSocialTrading/crypto-icons/master/src/images";
 
-export function getMessage() {
-  return 'hello'
-}
-
-export function sayMessage() {
-  console.log(getMessage())
-}
-
 export function getTokenImgUrl(token: string): string {
   switch (token) {
     case "inj":
-      return `${BASE_URL}/${token}.jpg`;
+      return `${BASE_URL}/${token}.png`;
     default:
       return "";
   }
