ae56999b6cbcab93b53fb88877a04c3991524e3e
assc-ryan <112598768+assc-ryan@users.noreply.github.com>
2023-01-12 17:37:43 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 const BASE_URL = "https://raw.githubusercontent.com/SocialSocialTrading/crypto-icons/master/src/images";
 
-export default function getTokenImgUrl(token: string): string {
+function getTokenImgUrl(token: string): string {
   switch (token) {
     case "inj":
       return `${BASE_URL}/${token}.jpg`;
     default:
       return "";
   }
+}
+
+export default {
+  getTokenImgUrl
 }
