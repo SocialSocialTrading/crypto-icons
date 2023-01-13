4b38b79a25083f966bb2aae3c993448082a52d32
assc-ryan <112598768+assc-ryan@users.noreply.github.com>
2023-01-13 11:06:35 +0800

    feat: add top 300 coins image

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
-const BASE_URL = "https://raw.githubusercontent.com/SocialSocialTrading/crypto-icons/master/src/images";
+const BASE_URL =
+  "https://raw.githubusercontent.com/SocialSocialTrading/crypto-icons/master/src/images";
 
 export function getTokenImgUrl(token: string): string {
-  switch (token) {
-    case "inj":
-      return `${BASE_URL}/${token}.png`;
-    default:
-      return "";
-  }
+  return `${BASE_URL}/${token}.png`;
 }
