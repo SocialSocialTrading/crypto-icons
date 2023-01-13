5ce6bd2de7268bea70fe2a68768abe630fca3c10
assc-ryan <112598768+assc-ryan@users.noreply.github.com>
2023-01-13 17:11:10 +0800

    feat: improve and export functions

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -4,17 +4,20 @@ interface IIconProp {
   name?: string;
 }
 
+export const getImageUrl = (name: string) => {
+  return new URL(
+    `../../assets/images/${name.toLowerCase()}.png`,
+    import.meta.url
+  ).href;
+};
+
+export const checkImageExist = (url: string): boolean =>
+  url.split("/").pop() !== "undefined";
+
 const Icon: FC<IIconProp> = ({ name }) => {
   if (!name) return null;
-
-  const getImageUrl = (name: string) => {
-    return new URL(
-      `../../assets/images/${name.toLowerCase()}.png`,
-      import.meta.url
-    ).href;
-  };
   const IMAGE_URL = getImageUrl(name);
-  return !(IMAGE_URL.split("/").pop() !== "undefined") ? null : <img src={IMAGE_URL} />;
+  return !checkImageExist(IMAGE_URL) ? null : <img src={IMAGE_URL} />;
 };
 
 export default Icon;
